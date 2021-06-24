917757d14ceb0b4492e32c346c8503c375497c08
Pascal Corpet <pcorpet@users.noreply.github.com>
2021-06-24 23:11:42 +0200

    fix: ensure complex object keys are quoted (#46)
    * fix: ensure complex object keys are quoted
    
    Fix #45
    
    * style: add spacing
    
    * style: spacing
    
    Co-authored-by: Keith Cirkel <keithamus@users.noreply.github.com>

--- test/objects.js
+++ test/objects.js
@@ -10,6 +10,23 @@ for (const [suite, inspect] of Object.entries({
       expect(inspect({})).to.equal('{}')
     })
 
+    it('quotes a key if it contains special chars', () => {
+      expect(inspect({ 'a.b': 1 })).to.equal("{ 'a.b': 1 }")
+      expect(inspect({ 'a b': 1 })).to.equal("{ 'a b': 1 }")
+    })
+
+    it('quotes a key if it is empty', () => {
+      expect(inspect({ '': 1 })).to.equal("{ '': 1 }")
+    })
+
+    it('quotes a key if it contains a single quote', () => {
+      expect(inspect({ "'": 1 })).to.equal("{ '\\'': 1 }")
+    })
+
+    it('quotes a key if it contains a double quote', () => {
+      expect(inspect({ '"': 1 })).to.equal("{ '\"': 1 }")
+    })
+
     if (suite === 'objects') {
       it('detects circular references', () => {
         const main = {}
