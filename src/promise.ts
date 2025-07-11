c2f6291cd5e10967db34ef0bee408f2cfbcecd98
James Garbutt <43081j@users.noreply.github.com>
2025-07-11 23:02:19 +0100

    feat: simplify promise inspection
    Dumbs it down to always return `Promise{...}` regardless of the state
    and platform/engine we're in, since Node has deprecated the private API
    we were using.

--- src/promise.ts
+++ src/promise.ts
@@ -1,19 +1,4 @@
 import type { Options } from './types.js'
 type GetPromiseValue = (value: Promise<unknown>, options: Options) => string
-let getPromiseValue: GetPromiseValue = () => 'Promise{…}'
-try {
-  // @ts-ignore
-  const { getPromiseDetails, kPending, kRejected } = process.binding('util')
-  if (Array.isArray(getPromiseDetails(Promise.resolve()))) {
-    getPromiseValue = (value, options: Options) => {
-      const [state, innerValue] = getPromiseDetails(value)
-      if (state === kPending) {
-        return 'Promise{<pending>}'
-      }
-      return `Promise${state === kRejected ? '!' : ''}{${options.inspect(innerValue, options)}}`
-    }
-  }
-} catch (notNode) {
-  /* ignore */
-}
+const getPromiseValue: GetPromiseValue = () => 'Promise{…}'
 export default getPromiseValue
